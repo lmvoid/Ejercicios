41b428599572839f1c961cc406d8d9554638424f
lmvoid <126919061+lmvoid@users.noreply.github.com>
2023-03-04 20:44:26 -0300

    Add files via upload

---
+++ Clase1/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+         for (int ultimoNum=14; ultimoNum>=5;ultimoNum--) {
+             int resi= ultimoNum%2;
+             if(resi<=0) {
+                 System.out.print(ultimoNum);
+                 System.out.print(",");
+             }
+             }
+             }
+        }
+
