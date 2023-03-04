30f4682ae7e0d19d4b3c3e61f1b60fa3ef23293d
lmvoid <126919061+lmvoid@users.noreply.github.com>
2023-03-04 20:29:38 -0300

    Add files via upload
    Clase1-Ejercicio2-InsisoD

---
+++ Clase1/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+       int ingreso=600000;
+       int vehiculos=2;
+       int inmuebles=4;
+       boolean otrosActivos=true;
+       if((ingreso>=572387)&&(vehiculos>=3)&&(inmuebles>=3)&&(otrosActivos=true)){
+           System.out.print("Usted pertenece al segmento de ingresos altos");
+       }else{
+           System.out.print("Usted no pertenece al segmento de ingresos altos");
+        }
+    }
+}
