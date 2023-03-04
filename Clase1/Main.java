fd673261475fffee7643b46b53dbcfacc1437962
lmvoid <126919061+lmvoid@users.noreply.github.com>
2023-03-04 20:38:50 -0300

    Add files via upload

---
+++ Clase1/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 14;
+        while(num1<=num2){
+            System.out.print(num1);
+            System.out.print(",");
+            num1= num1+1;
+
+    }
+
+
+    }
+}
